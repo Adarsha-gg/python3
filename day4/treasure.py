b5e047273c123179a55db087e7fa8e568a6903a3
Scarz <heroicadu@gmail.com>
2022-06-25 13:07:26 +0545

    added code

---
+++ day4/treasure.py
@@ -0,0 +1,10 @@
+
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print("Enterr the coordinates")
+column = int(input("Enter the column"))
+row = int(input("Enter the row"))
+map[column][row] = "X"
+print(f"{row1}\n{row2}\n{row3}")
