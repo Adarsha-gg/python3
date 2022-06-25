68aa4dd115a0089d334898f7a46493dac98de7cf
Scarz <heroicadu@gmail.com>
2022-06-25 12:53:30 +0545

    initial commit

---
+++ day1/project.py
@@ -0,0 +1,4 @@
+print("Band Name generator")
+a = input("What's name of the city you grew up in: ")
+b = input("What's your pet name: ")
+print(f"Your band name could be {a} {b}")
---
+++ day2/day2proj.py
@@ -0,0 +1,18 @@
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill? Rs."))
+tip = int(input("what percentage tip do u want to give? 10,15,12 :"))
+splitt = int(input("How many people to split: "))
+
+
+if (tip == 10):
+    total =(0.1*bill+bill)/splitt
+    
+elif (tip == 12):
+    total =(0.12*bill+bill)/splitt    
+    
+elif (tip == 15):
+    total =(0.15*bill+bill)/splitt
+        
+   
+       
+print(f"Each person should pay Rs.{total} ")       
