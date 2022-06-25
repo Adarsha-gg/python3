09c32cf48360c77be19c440596f39ded8f1fff8e
Scarz <heroicadu@gmail.com>
2022-06-25 13:07:20 +0545

    var change

--- day4/billroullette.py
+++ day4/billroullette.py
@@ -2,7 +2,7 @@ import random
 
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-intz = random.randint(0,len(names)-1)
-print(f"{names[intz]} is going to pay the bill")
+int = random.randint(0,len(names)-1)
+print(f"{names[int]} is going to pay the bill")
 
 
