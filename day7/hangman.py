0e0cc38a298ecee1fc174786455346e6e2be6ebc
Scarz <heroicadu@gmail.com>
2022-06-26 23:22:23 +0545

    hangman

---
+++ day7/hangman.py
@@ -0,0 +1,86 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+win = False
+word = ["aardvark", "baboon", "camel"]
+main = random.choice(word)
+lives =6
+g = len(main)
+display = []
+
+for i in range(g):
+    display +="_"
+
+while(lives != 0 or win == False):
+    guess = input("Enter ur guess: ").lower()
+    for position in range(g):   
+        letter = main[position]
+        if(guess == letter):
+            display[position] = letter
+           
+    if guess not in main:
+        lives -=1
+        if lives == 0:
+            win = True
+            print("You lose")
+            
+    print(f"{' '.join(display)}")
+    if"_" not in display:
+        win = True
+        print("Yoou win")
+    print(stages[lives])
