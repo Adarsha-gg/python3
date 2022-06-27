c89fdfce020713cb946ef8a806afcdda37c1bc8c
Scarz <heroicadu@gmail.com>
2022-06-27 18:21:29 +0545

    area calc

---
+++ day8/areacalc.py
@@ -0,0 +1,21 @@
+import math
+#Write your code below this line 👇
+def paint_calc(height,width,cover):
+    cans = (height*width)/coverage
+    print(f"you need {math.ceil(cans)} number of paint") 
+
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+h = int(input("Height of wall: "))
+w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=h, width=w, cover=coverage)
+
+
