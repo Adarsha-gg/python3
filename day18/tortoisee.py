cd85e3a0620d4bf242490d9af9238d8259b7233e
Scarz <heroicadu@gmail.com>
2022-07-18 23:37:28 +0545

    test

---
+++ day18/tortoisee.py
@@ -0,0 +1,16 @@
+from turtle import Screen, Turtle
+
+tim = Turtle()
+tim.shape("circle")
+tim.color("blue violet")
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
