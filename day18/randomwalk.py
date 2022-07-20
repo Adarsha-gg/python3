0264172003be9e40fe9ff562360390b98be4ea45
Scarz <heroicadu@gmail.com>
2022-07-20 23:53:09 +0545

    added rgb

--- day18/randomwalk.py
+++ day18/randomwalk.py
@@ -4,16 +4,24 @@ stop = False
 tim = t.Turtle()
 tim.shape("arrow")
 tim.color("black")
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
 move =[90,180,270,360]
 tim.speed("fast")
-tim.pensize(5)
+tim.pensize(13)
+t.colormode(255)
+def rcolor():
+    r = random.randint(1,255)
+    g = random.randint(1,255)
+    b = random.randint(1,255)
+    color = (r, g, b)
+    return color
 
 def movee():
     g = random.choice(move)
+    ez = rcolor()
     tim.left(g)
     tim.forward(40)
-    tim.color(random.choice(colours))
+    tim.color(ez)
 
 while stop != True:
     movee()
