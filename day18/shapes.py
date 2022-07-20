176275791af80dce5a480a5311d23742999b9828
Scarz <heroicadu@gmail.com>
2022-07-20 23:48:48 +0545

    added rgb

--- day18/shapes.py
+++ day18/shapes.py
@@ -1,44 +1,49 @@
-from turtle import Screen, Turtle
+import turtle as t
 import random
 
-
-tim = Turtle()
+t.colormode(255)
+tim = t.Turtle()
 tim.shape("arrow")
 tim.color("black")
 tim.speed("fastest")
 sides = 3
-while sides < 10:
+tim.pensize(5)
+def rcolor():
     r = random.randint(1,255)
     g = random.randint(1,255)
     b = random.randint(1,255)
-    #The rgb feature didnt work sadly F
+    color = (r, g, b)
+    return color
+
+while sides < 10:
+   
+    g = rcolor()
+   
     for i in range(sides):
         tim.right(360/sides)
         tim.forward(50)
-
+        tim.color(g)
     sides += 1
 
 while sides != 2:
-    r = random.randint(1,255)
-    g = random.randint(1,255)
-    b = random.randint(1,255)
-    #The rgb feature didnt work sadly F
+    g = rcolor()
+    
     for i in range(sides):
         tim.right(360/sides)
         tim.backward(50)
-
+        tim.color(g)
     sides -= 1
     
 tim.left(180)
 tim.forward(200)
 
 while sides != 10:
+    g = rcolor()
     
-    #The rgb feature didnt work sadly F
     for i in range(sides):
         tim.right(360/sides)
         tim.backward(50)
-
+        tim.color(g)
     sides += 1    
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
