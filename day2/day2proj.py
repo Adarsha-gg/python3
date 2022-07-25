fc666b4272f6d21428238fd63164da10c0beeca6
Scarz <heroicadu@gmail.com>
2022-07-26 02:09:58 +0545

    improved

--- day2/day2proj.py
+++ day2/day2proj.py
@@ -1,18 +1,11 @@
 print("Welcome to the tip calculator")
-bill = float(input("What was the total bill? Rs. "))
-tip = int(input("what percentage tip do u want to give? 10,13,15 :"))
-splitt = int(input("How many people to split: "))
+bill=float(input("What is the total bill?\n"))
+people=int(input("How many people to split the bill?\n"))
+tip=int(input("What percent would you like to tip?\n"))
+
+bill= (bill+(bill*tip/100))/people
 
 
-if (tip == 10):
-    total =(0.1*bill+bill)/splitt
-    
-elif (tip == 12):
-    total =(0.12*bill+bill)/splitt    
-    
-elif (tip == 15):
-    total =(0.15*bill+bill)/splitt
-        
-   
-       
-print(f"Each person should pay Rs.{total} ")       
+amount=round (bill, 2)
+
+print(f"Each person has to pay ${amount}.")
