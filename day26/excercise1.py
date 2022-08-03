dbddbf6fa4a4d7ab664e0a7dba017306e2cf62f1
Scarz <heroicadu@gmail.com>
2022-08-03 22:40:57 +0545

    nice

---
+++ day26/excercise1.py
@@ -0,0 +1,14 @@
+# range = [n*2 for n in range(1,5)]
+# print(range)
+# ans = "hero"
+# names = ["alex","beth","caroline","dave","freddie"]
+
+# new_names = [namee.upper() for namee in names if (len(namee) > 5)]
+# print(new_names)
+
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [number*number for number in numbers]
+
+
+print(squared_numbers)
