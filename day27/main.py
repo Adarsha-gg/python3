5661c1b27c09ed121e09e80260d38962df0834b4
Scarz <heroicadu@gmail.com>
2022-08-05 01:47:01 +0545

    complete

--- day27/main.py
+++ day27/main.py
@@ -1,42 +1,37 @@
-from cgitb import text
 import tkinter
 
+#main display screen
 
 window = tkinter.Tk()
-window.title("Hi") #windows name
-window.minsize(width=600 , height= 600) 
-
-#Label
-
-label = tkinter.Label(text="LETS GO LABEL", font=("Arial",22))
-label.pack()  #display into the screen
+window.title("Mile to Km converter") #windows name
+window.minsize(width=600 , height= 600)
+window.config(padx= 10 , pady= 10) 
 
 #for clicking
 def click():
+    miles = int(entry.get())
+    km = miles*1.6
+    label4.config(text= km)
 
-     label.config(text = entry.get()) 
+#entry
+entry = tkinter.Entry()
+entry.grid(row=0, column= 2)
 
+#Label
+label = tkinter.Label(text="Miles", font=("Arial",10))
+label.grid(column =  3, row = 0)  #display into the screen
+
+label2 = tkinter.Label(text="is equal to", font=("Arial",12))
+label2.grid(column =  1, row = 1)  #display into the screen
+
+label3 = tkinter.Label(text="KM", font=("Arial",10))
+label3.grid(column =  3, row = 1)  #display into the screen
+
+label4 = tkinter.Label(text="0", font=("Arial",10))
+label4.grid(column =  2, row = 1)
 
 #button
-button = tkinter.Button(text="helo" , background="black", command= click)
-button.pack()
-
-
-#ENTRY
-entry = tkinter.Entry(width=20)
-entry.pack()
-
-
-
-
-
-
-
-
-
-
-
-
-
+button = tkinter.Button(text="Calculate" , command= click)
+button.grid(column=3 , row= 3)
 
 window.mainloop()  #always at the end
