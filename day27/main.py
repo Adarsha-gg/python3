a91377bd29d3fd7b5a722b588ef55f3b688baaff
Scarz <heroicadu@gmail.com>
2022-08-04 23:45:23 +0545

    test

---
+++ day27/main.py
@@ -0,0 +1,29 @@
+import tkinter
+
+
+window = tkinter.Tk()
+window.title("Hi")
+window.minsize(width=600 , height= 600)
+
+#Label
+
+label = tkinter.Label(text="LETS GO LABEL", font=("Arial",13))
+label.pack()  #display into the screen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()  #always at the end
