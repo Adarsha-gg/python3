3f79cc137680bb2f00b90b5d67fe2634a8d47b8f
Scarz <heroicadu@gmail.com>
2022-08-15 02:02:58 +0545

    kanye

--- day33/main.py
+++ day33/main.py
@@ -1,18 +1,31 @@
-#100 - wait
-#200 - confirm
-#300 - u dont have acess
-#400 - the req doesnt exist (user fault)
-#500 - the server is down
 
-
+from cgitb import text
+from tkinter import *
 import requests
+data ={}
+def get_quote():
+   kanye = requests.get("https://api.kanye.rest")
+   kanye.raise_for_status()
+   quote = kanye.json()
+   data = quote["quote"]
+   canvas.itemconfig(quote_text,text=data)
+   
 
-response =requests.get("http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
 
-data =response.json()
-latitude = data['iss_position']['latitude']
-longitude = data['iss_position']['longitude']
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
 
-iss=(latitude,longitude)
-print(iss)
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="day33/background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="" ,width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="day33\kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
